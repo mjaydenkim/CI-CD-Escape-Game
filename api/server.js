44113d55ae74645c635f71fa3b320c0a9a3b049b
Matthew Kim <mk2672@cornell.edu>
2025-11-22 15:30:54 -0500

    Revert "Introduce production bug :("
    This reverts commit cb42cd756d86eb127650e07b4aae7337094b1ee5.

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/ping", (req, res) => {
-  throw new Error("Production bug!");
+  res.send("pong");
 });
 
 app.listen(3000, () => console.log("API running"));
